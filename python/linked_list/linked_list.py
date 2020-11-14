46ca8d77408d3dea1b0ad3d34ec4893c0271d004
Brendan Welzien <brendanwelzien@gmail.com>
2020-11-13 18:42:25 -0800

    tests passing

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,14 +1,33 @@
 
 class LinkedList:
 
-    def __init__(self):
+    def __init__(self): # head property that is empty
+        self.head = None
 
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value): # adds a node to the head
+        self.head = Node(value, self.head)
+
+    def includes(self, value): # return boolean if value exists
+        currentPlace = self.head
+        while currentPlace:
+            if currentPlace.value == value:
+                return True
+            currentPlace = currentPlace.next_n
+        return False
+
+    def to_string(self):
+        currentPlace = self.head
+        string_of_values = ""
+        while currentPlace:
+            string_of_values+= f" { {str(currentPlace.value)} } ->"
+            currentPlace = currentPlace.next_n # move on to next node
+        return string_of_values.replace("'"," ") + " NULL" # final string
+
+    def __repr__(self):
+        return self.to_string()
 
 class Node:
     def __init__(self, value, next_n=None): # next = pointer to next node
         self.value=value
-        self.next_n=next_n 
+        self.next_n=next_n
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,6 +1,51 @@
 import pytest
 from linked_list.linked_list import LinkedList
 
+def test_instance(): #instance check
+    assert LinkedList()
 
-def test_import():
-    assert LinkedList
+def test_LinkedList_insert():
+    L = LinkedList()
+    L.insert("a")
+    L.insert("b")
+    assert L.head.value == "b"
+    assert L.head.next_n.value == "a"
+
+def test_LinkedList_head():
+    L = LinkedList()
+    assert L.head == None
+
+def test_LinkedList_multiple():
+    L = LinkedList()
+    L.insert("c")
+    L.insert("b")
+    L.insert("a")
+
+    actual = str(L)
+    expected =" { a } -> { b } -> { c } -> NULL"
+    assert actual == expected
+
+def test_LinkedList_true():
+    L = LinkedList()
+    L.insert("a")
+    L.insert("b")
+    L.insert("c")
+    actual = L.includes("c")
+    expected = True
+    assert actual == expected
+
+def test_LinkedList_false():
+    L = LinkedList()
+    L.insert("a")
+    L.insert("b")
+    L.insert("c")
+    actual = L.includes("d")
+    expected = False
+    assert actual == expected
+
+def test_LinkedList_collection():
+    L = LinkedList()
+    L.insert("a")
+    L.insert("b")
+    L.insert("c")
+    return L

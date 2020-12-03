010c7776bd7d3793ae6643a56c163ad21ada86e1
Brendan Welzien <brendanwelzien@gmail.com>
2020-12-02 19:15:29 -0800

    fixed code challenge from earlier, test failing for this one

--- python/code_challenges/tree/tree.py
+++ python/code_challenges/tree/tree.py
@@ -1,4 +1,4 @@
-
+from collections import deque
 class Node:
     def __init__(self, value, left=None, right=None):
         self.left = left
@@ -44,6 +44,64 @@ class BinaryTree:
             values.append(position.value)
         walk(self.root)
         return values
+
+    def add(self, value):
+        new_n = Node(value)
+        queue = Queues()
+        if self.root != None:
+            queue.enqueue(self.root)
+
+            while not queue.is_empty():
+                top = queue.dequeue()
+                if not top.left:
+                    top.left = new_n
+                    return
+                if top.right == None:
+                    top.right = new_n
+                    return
+                if top.left:
+                    queue.enqueue(top.left)
+                if top.right:
+                    queue.enqueue(top.right)
+        else:
+            self.root = new_n
+        return
+
+
+    def breadth_first(self): # goes from top to bottom and left to right for each tree row
+        queue = Queues()
+        collection = []
+        queue.enqueue(self.root) # start at top
+
+        while not queue.is_empty():
+            top = queue.dequeue()
+            collection.append(top.value)
+            if collection.left != None:
+                queue.enqueue(top.left)
+            if collection.right != None:
+                queue.enqueue(top.right)
+        return collection
+
+
+class Queues:
+    def __init__(self):
+        self.container = deque()
+
+    def is_empty(self):
+        len(self.container) == 0
+        return
+
+    def enqueue(self, value):
+        self.container.appendleft(value)
+
+    def dequeue(self):
+        self.container.pop()
+        return
+
+    def peek(self):
+        if self.container:
+            return self.container[-1]
+
 
 class BinarySearchTree(BinaryTree):
     def add(self, value):

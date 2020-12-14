6901ccd01d774e75a55f4a0de5b6b58ec4960e82
Brendan Welzien <brendanwelzien@gmail.com>
2020-12-14 14:19:38 -0800

    added test edge cases

--- python/code_challenges/insertion-sort/insertion_sort.py
+++ python/code_challenges/insertion-sort/insertion_sort.py
@@ -3,7 +3,7 @@ def insert_sort(list):
         index_position = index
         temporary_position = list[index]
 
-        while index_position > 0 and temporary_position < list[index_position - 1]:
+        while index_position > 0 and temporary_position < list[index_position - 1]: # comparing 8 and 4 for example
             list[index_position] = list[index_position - 1]
             index_position -= 1
         list[index_position] = temporary_position
@@ -11,4 +11,10 @@ def insert_sort(list):
 
 if __name__ == "__main__":
     sortify = insert_sort([8, 4, 23, 42, 16, 15])
+    reverse_sort = insert_sort([20, 18, 12, 8, 5, -2])
+    few_unique = insert_sort([5, 12, 7, 5, 5, 7])
     print(sortify)
+    print(reverse_sort)
+    print(few_unique)
+
+

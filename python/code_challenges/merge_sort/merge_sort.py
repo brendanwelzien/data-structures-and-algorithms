e6b5a303cbf4c41e59063e88b9aa88c5836da877
Brendan Welzien <brendanwelzien@gmail.com>
2020-12-15 18:31:28 -0800

    merge sort

---
+++ python/code_challenges/merge_sort/merge_sort.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+  if len(arr) > 1:
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+    i = 0 # left half
+    j = 0 # right half
+    k = 0 # whole arr iterator
+
+    while i < len(left) and j < len(right):
+      if left[i] <= right[j]:
+        arr[k] = left[i]
+        i += 1
+      else:
+        arr[k] = right[j]
+        j += 1
+      k += 1
+
+    # remaining
+    while i < len(left):
+      arr[k] = left[i] # position of k index is at left index now
+      k += 1
+      i += 1
+
+    while j < len(right):
+      arr[k] = right[j]
+      k += 1
+      j += 1
+  return arr
+
+if __name__ == "__main__":
+  arr = [8, 4, 23, 42, 16, 15]
+  merge_sort(arr)
+  print(arr)
